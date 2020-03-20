3732333b04238af3aaad5331b50a9caff90d670b
Sitael <beru666@live.fr>
2020-03-20 16:38:57 +0100

    add commentaires on index.js

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ app.get("/get_text", (req, res)=> {
 
     });
 });
+
+// Commande à mettre dans la console pour test si cela fonctionne
+// const divContent = document.getElementById('content');
+
+// const response = await fetch ('get_text');
+
+// const json = await response.json();
+
+// const name = divContent.getElementsByClassName('name')[0];
+// const verified = divContent.getElementsByClassName('boolean')[0];
+// const birthdate = divContent.getElementsByClassName('birthday')[0];
+// const friends = divContent.getElementsByClassName('array')[0];
+
+// name.innerHTML = json.name;
+// verified.innerHTML = json.verified;
+// birthdate.innerHTML = json.birthdate;
+// friends.innerHTML = json.friends;
